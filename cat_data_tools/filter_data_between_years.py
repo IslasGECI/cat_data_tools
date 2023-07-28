49fbd839aef78796c3eb7ce67c1ed4191891813c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-28 21:15:45 +0000

    ✅ Pass tests

--- cat_data_tools/filter_data_between_years.py
+++ cat_data_tools/filter_data_between_years.py
@@ -1,22 +1,4 @@
 import pandas as pd
-import click
-from click_default_group import DefaultGroup
-
-
-@click.group(cls=DefaultGroup, default="create", default_if_no_args=True)
-def cli():
-    pass
-
-
-@cli.command(short_help="Filter data from Socorro Island Eradication Project")
-@click.option("--input-path", "-r", type=click.Path(), help="Input file path")
-@click.option("--output-path", "-w", type=click.Path(), help="Output file path")
-@click.option("--initial-year", "-i", type=int, help="Initial year")
-@click.option("--final-year", "-f", default=3000, type=int, help="Final year")
-def write_yearly_summary(input_path, output_path, initial_year, final_year):
-    dataframe = pd.read_csv(input_path)
-    filtered_dataframe = filter_data_between_years(dataframe, initial_year, final_year)
-    filtered_dataframe.to_csv(output_path, index=False, na_rep="NA")
 
 
 def filter_data_between_years(monthly_data, initial_year, final_year=3000):
