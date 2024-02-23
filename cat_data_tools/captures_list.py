0804947e94958a996e53d34176f54dfa8e5b4af0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-23 18:52:24 +0000

    ✅ Pass tests

--- cat_data_tools/captures_list.py
+++ cat_data_tools/captures_list.py
@@ -1,5 +1,5 @@
 def join_trap_info_with_captures(captures, traps_info):
-    return captures
+    return captures.join(traps_info.set_index("ID_de_trampa"), on="ID_de_trampa")
 
 
 def select_captures_from_daily_status(daily_status):
