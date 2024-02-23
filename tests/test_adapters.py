f5f72f6d1af53ac30d26fa82a13bf697a4c83e55
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-23 22:49:25 +0000

    🛑🧪 Fail tests

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -6,7 +6,7 @@ def test_adapter_path_to_dataframe():
     path = "tests/data/trap_daily_status.csv"
     adapter = Adapter_for_path_to_dataframe(path)
     obtained = adapter.get_dataframe()
-    assert obtained == "from_csv"
+    assert isinstance(obtained, pd.DataFrame)
 
     path = ""
     adapter = Adapter_for_path_to_dataframe(path)
