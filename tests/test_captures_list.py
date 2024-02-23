e87dfd7f0ab1b1f60d79a5b6f7cb60c707eece69
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-23 18:44:22 +0000

    ♻️  Refactor

--- tests/test_captures_list.py
+++ tests/test_captures_list.py
@@ -10,18 +10,18 @@ def test_select_captures_from_daily_status():
 
 
 def tests_join_trap_info_with_captures():
-    traps_info = pd.DataFrame(
-        {
-            "ID_de_trampa": ["TC-01-2156-MV", "TC-02-2156-MO", "TC-01-2157-NR"],
-            "latitude": [19, 20, 21],
-            "longitude": [-118, -120, -121],
-        }
-    )
-
     obtained = join_trap_info_with_captures(captures, traps_info)
     expected_len = 3
     assert len(obtained) == expected_len
 
+
+traps_info = pd.DataFrame(
+    {
+        "ID_de_trampa": ["TC-01-2156-MV", "TC-02-2156-MO", "TC-01-2157-NR"],
+        "latitude": [19, 20, 21],
+        "longitude": [-118, -120, -121],
+    }
+)
 
 captures = pd.DataFrame(
     {
