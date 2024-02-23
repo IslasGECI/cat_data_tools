50f6d653e523cec1d6a748d52f42e89a1d1d3962
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-23 19:00:18 +0000

    ♻️  Refactor

--- tests/test_captures_list.py
+++ tests/test_captures_list.py
@@ -11,12 +11,16 @@ def test_select_captures_from_daily_status():
 
 def tests_join_trap_info_with_captures():
     obtained = join_trap_info_with_captures(captures, traps_info)
+    assert_obatined_has_all_captures(obtained)
+    assert_obtained_has_coordinates_columns(obtained)
+
+
+def assert_obatined_has_all_captures(obtained):
     expected_len = 3
     assert len(obtained) == expected_len
-    assert_the_joined_dataframe_has_coordinates_columns(obtained)
 
 
-def assert_the_joined_dataframe_has_coordinates_columns(obtained):
+def assert_obtained_has_coordinates_columns(obtained):
     expected_columns = ["latitude", "longitude"]
     are_expected_columns_in_obtained = [
         expected_column in obtained.columns.values for expected_column in expected_columns
