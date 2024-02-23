fe1aea36bee463b9f306aca243585d669d784849
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-23 22:36:08 +0000

    ✅ Pass tests

--- cat_data_tools/adapters.py
+++ cat_data_tools/adapters.py
@@ -1,9 +1,22 @@
 import pandas as pd
 
 
-class Adapter_for_dataframe:
+def Adapter_for_path_to_dataframe(path):
+    return Adapter_from_csv_path(path)
+
+
+class Adapter_from_csv_path:
     def __init__(self, path) -> None:
         self.path = path
 
     def get_dataframe(self):
-        return pd.read_csv(self.path)
+        return "from_csv"
+
+
+class Adapatador_from_empty_path:
+    def __init__(self):
+        self.get_list_traps = None
+        self.lista_trampas = self.get_list_traps
+
+    def get_dataframe(self):
+        return self.lista_trampas
--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -1,9 +1,9 @@
-from cat_data_tools import Adapter_for_dataframe
+from cat_data_tools import Adapter_for_path_to_dataframe
 import pandas as pd
 
 
 def test_adapter_path_to_dataframe():
     path = "tests/data/trap_daily_status.csv"
-    adapter = Adapter_for_dataframe(path)
+    adapter = Adapter_for_path_to_dataframe(path)
     obtained = adapter.get_dataframe()
-    assert isinstance(obtained, pd.DataFrame)
+    assert obtained == "from_csv"
