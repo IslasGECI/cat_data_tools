c23bcba14c3e7927a909181c41f9e152106650b2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-13 21:01:03 +0000

    ✅ Pass tests

--- cat_data_tools/trap_status_list.py
+++ cat_data_tools/trap_status_list.py
@@ -1,3 +1,6 @@
 def join_trap_ids_and_daily_status(trap_daily_status, trap_ids):
-    joined_dataframe = trap_daily_status.join(trap_ids.set_index("ID"), on="ID_de_trampa")
+    trap_daily_status_without_na = trap_daily_status.dropna(axis=0, subset=["Estado_trampa"])
+    joined_dataframe = trap_daily_status_without_na.join(
+        trap_ids.set_index("ID"), on="ID_de_trampa"
+    )
     return joined_dataframe
