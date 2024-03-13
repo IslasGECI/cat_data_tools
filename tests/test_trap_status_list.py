a279afae89db7c96c8e267f814a069acd2788e9b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-13 20:58:48 +0000

    🛑🧪 Fail tests

--- tests/test_trap_status_list.py
+++ tests/test_trap_status_list.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 
 def test_join_trap_ids_and_daily_status():
-    trap_daily_status = pd.read_csv("tests/data/trap_daily_status.csv")
+    trap_daily_status = pd.read_csv("tests/data/daily_status_trap_guadalupe.csv")
     trap_ids = pd.read_csv("tests/data/traps_ids_latlon.csv")
-    join_trap_ids_and_daily_status(trap_daily_status, trap_ids)
+    obtained = join_trap_ids_and_daily_status(trap_daily_status, trap_ids)
+    print(obtained)
+    assert len(obtained) == len(trap_daily_status) - 1
+    assert len(obtained.columns) == 6
