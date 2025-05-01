decdccffbfbd0b852b9af599e6998d4caaf94481
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-01 20:30:23 +0000

    ✅ Pass tests

--- cat_data_tools/update_status_traps.py
+++ cat_data_tools/update_status_traps.py
@@ -1,2 +1,4 @@
-def update_status_traps(traps_info_df_cutted):
-    pass
+def update_status_traps(traps_info_df):
+    return traps_info_df.drop_duplicates(subset=["Tipo", "ID", "Orden"], keep="first").reset_index(
+        drop=True
+    )
