90789ecf643c5e15cb62f762a0d5d49fa9db649f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-08 18:11:02 +0000

    ✅ Pass tests

--- cat_data_tools/update_status_traps.py
+++ cat_data_tools/update_status_traps.py
@@ -1,6 +1,10 @@
 def _update_status_traps(traps_info_df):
-    df = traps_info_df.sort_values(by="Fecha")
+    df = traps_info_df.sort_values(by=["ID", "Fecha"])
     is_type_changed = df["Tipo"] != df["Tipo"].shift()
+    is_order_changed = df["Orden"] != df["Orden"].shift()
     is_trap_status_first_apparence = ~df.duplicated(subset=["Tipo", "ID", "Orden"], keep="first")
-    filtered_df = df.loc[is_type_changed | is_trap_status_first_apparence]
+    print(is_type_changed, "is_type_changed")
+    print(is_order_changed, "is_order_changed")
+    print(is_trap_status_first_apparence, "is_trap_status_first_apparence")
+    filtered_df = df.loc[is_type_changed | is_order_changed | is_trap_status_first_apparence]
     return filtered_df.reset_index(drop=True)
--- tests/test_update_status_traps.py
+++ tests/test_update_status_traps.py
@@ -6,22 +6,36 @@ import pandas as pd
 def test_update_status_traps():
     traps_info_df = pd.DataFrame(
         {
-            "Tipo": ["TC", "TC", "TC", "TL", "TC"],
-            "ID": ["10-0008", "10-0006", "10-0006", "10-0006", "10-0006"],
-            "Orden": [8, 8, 8, 8, 8],
-            "Fecha": ["2025-02-02", "2025-02-02", "2025-02-09", "2025-02-16", "2025-02-23"],
+            "Tipo": ["TC", "TC", "TC", "TL", "TC", "TC"],
+            "ID": ["10-0008", "10-0006", "10-0006", "10-0006", "50-0006", "10-0006"],
+            "Orden": [8, 8, 8, 8, 3, 8],
+            "Fecha": [
+                "2025-02-02",
+                "2025-02-02",
+                "2025-02-09",
+                "2025-02-16",
+                "2025-02-16",
+                "2025-02-23",
+            ],
         }
     )
     traps_info_df_cutted = traps_info_df.iloc[0:4]
+    print(traps_info_df_cutted)
     obtained = _update_status_traps(traps_info_df_cutted)
+    print(obtained)
     expected_number_of_rows = 3
     assert (
         obtained.shape[0] == expected_number_of_rows
     ), f"Expected {expected_number_of_rows} rows, but got {obtained.shape[0]} rows"
-    assert obtained.iloc[1]["Fecha"] == "2025-02-02"
+    assert obtained.iloc[0]["Fecha"] == "2025-02-02"
 
     obtained = _update_status_traps(traps_info_df)
-    expected_number_of_rows = 4
+    print(obtained)
+    expected_number_of_rows = 5
     assert (
         obtained.shape[0] == expected_number_of_rows
     ), f"Expected {expected_number_of_rows} rows, but got {obtained.shape[0]} rows"
+
+    traps_info_df = pd.read_csv("tests/data/traps_info_for_tests.csv")
+    obtained = _update_status_traps(traps_info_df)
+    assert len(obtained) == 5
