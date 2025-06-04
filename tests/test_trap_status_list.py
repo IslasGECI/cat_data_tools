bfd25b5fca00b9217d969f0804699e6ce85e42f7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-04 18:11:55 +0000

    🛑🧪 Fail tests

--- tests/test_trap_status_list.py
+++ tests/test_trap_status_list.py
@@ -1,4 +1,7 @@
-from cat_data_tools import join_trap_ids_and_daily_status
+from cat_data_tools.trap_status_list import (
+    join_trap_ids_and_daily_status,
+    add_lat_lon_to_active_traps_of_the_week,
+)
 
 import pandas as pd
 
@@ -9,3 +12,9 @@ def test_join_trap_ids_and_daily_status():
     obtained = join_trap_ids_and_daily_status(trap_daily_status, trap_ids)
     assert len(obtained) == len(trap_daily_status) - 1
     assert len(obtained.columns) == 6
+
+
+def tests_add_lat_lon_to_active_traps_of_the_week():
+    active_traps = pd.read_csv("tests/data/active_traps_splitted.csv")
+    traps_positions_with_lat_lon = pd.read_csv("tests/data/traps_positions_with_latlon.csv")
+    obtained = add_lat_lon_to_active_traps_of_the_week(active_traps, traps_positions_with_lat_lon)
