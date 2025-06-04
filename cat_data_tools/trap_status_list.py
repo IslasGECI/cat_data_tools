cd2208e454c8fe2f6b288f6e180f7af383c1f102
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-04 18:18:58 +0000

    ✅ Pass tests

--- cat_data_tools/trap_status_list.py
+++ cat_data_tools/trap_status_list.py
@@ -7,4 +7,7 @@ def join_trap_ids_and_daily_status(trap_daily_status, trap_ids):
 
 
 def add_lat_lon_to_active_traps_of_the_week(active_traps, traps_positions_with_lat_lon):
-    pass
+    joined = active_traps.join(
+        traps_positions_with_lat_lon.set_index(["ID", "Linea"]), on=["ID_de_trampa", "line"]
+    )
+    return joined
