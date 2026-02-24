00cc0617bd9239643bdc16a507023edb054dd0da
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-24 18:27:36 +0000

    ✅ Pass tests

--- cat_data_tools/transform_effort_captures.py
+++ cat_data_tools/transform_effort_captures.py
@@ -1,4 +1,5 @@
 def transform_effort_captures_to_dict(df):
+    df["Esfuerzo"] = df.Esfuerzo.replace(0, 1)
     effort_captures_dict = (
         df[["Esfuerzo", "Capturas"]]
         .rename(columns={"Esfuerzo": "esfuerzo", "Capturas": "capturas"})
