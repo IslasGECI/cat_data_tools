02c3244731220a796c807390e8db48bade098cd1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-24 22:38:27 +0000

    ✅ Pass tests

--- cat_data_tools/build_trap_daily_status.py
+++ cat_data_tools/build_trap_daily_status.py
@@ -5,10 +5,8 @@ def build_trap_daily_status(check_traps_log_df):
     df = check_traps_log_df.copy()
     df["Date"] = pd.to_datetime(df["Date"])
 
-    df_activos = df[df["Trap_status"] == "A"].copy()
     days_active = 7
-    df_activos["Date"] = df_activos["Date"].apply(
-        lambda x: pd.date_range(start=x, periods=days_active, freq="D")
-    )
-    df_exploded = df_activos.explode("Date")
-    return df_exploded.drop_duplicates(subset=["ID", "Date"])
+    df["Date"] = df["Date"].apply(lambda x: pd.date_range(start=x, periods=days_active, freq="D"))
+    df_exploded = df.explode("Date")
+    df_exploded.drop_duplicates(subset=["ID", "Date"], keep="last", inplace=True)
+    return df_exploded[df_exploded["Trap_status"] == "A"].copy()
