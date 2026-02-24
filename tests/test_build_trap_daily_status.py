4476bce4ab0bc5e3c955198566b5a9f4b099c29f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-24 21:34:42 +0000

    🛑🧪 Fail tests

--- tests/test_build_trap_daily_status.py
+++ tests/test_build_trap_daily_status.py
@@ -7,3 +7,5 @@ def tests_build_trap_daily_status():
     check_traps_log_df = pd.read_csv("tests/data/check_traps_log.csv")
     obtained = build_trap_daily_status(check_traps_log_df)
     assert isinstance(obtained, pd.DataFrame)
+    expected_days_for_trap_001 = 14
+    assert len(obtained[obtained["ID"] == "10-001"]) == expected_days_for_trap_001
