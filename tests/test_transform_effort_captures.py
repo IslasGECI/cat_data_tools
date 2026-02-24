8cff51e4c99e5ac3352a5f24651085d5971cd4ae
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-24 18:16:12 +0000

    🛑🧪 Fail tests

--- tests/test_transform_effort_captures.py
+++ tests/test_transform_effort_captures.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 def test_transform_effort_captures_to_dict():
+    df = pd.read_csv("tests/data/esfuerzo_capturas_mensuales_gatos_socorro_3_years.csv")
     obtained = transform_effort_captures_to_json(df)
     expected_keys = ["T", "esfuerzo", "capturas"]
     assert list(obtained.keys()) == expected_keys
