7f8b4bd263ba138a3528663a8add8d2e94327199
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 22:48:54 +0000

    🛑🧪 Fail tests

--- tests/test_build_trap_daily_status.py
+++ tests/test_build_trap_daily_status.py
@@ -1,12 +1,13 @@
 from cat_data_tools.build_trap_daily_status import build_trap_daily_status
 import pandas as pd
 
+active_days = 7
+
 
 def tests_build_trap_daily_status():
-
     check_traps_log_df = pd.read_csv("tests/data/check_traps_log.csv")
-    active_days = 7
     obtained = build_trap_daily_status(check_traps_log_df, active_days)
+    obtained.to_csv("salida.csv", index=False)
     assert isinstance(obtained, pd.DataFrame)
     expected_days_for_trap_001 = 14
     assert len(obtained[obtained["ID"] == "10-001"]) == expected_days_for_trap_001
@@ -28,3 +29,14 @@ def tests_build_trap_daily_status():
     assert len(obtained[obtained["ID"] == "10-008"]) == expected_days_for_trap_008
     expected_days_for_trap_009 = 4
     assert len(obtained[obtained["ID"] == "10-009"]) == expected_days_for_trap_009
+
+
+def test_build_trap_daily_status_for_duplicated_positions():
+    check_traps_log_with_duplicated_positions = pd.read_csv(
+        "tests/data/check_traps_log_with_duplicated_position_id.csv"
+    )
+    obtained = build_trap_daily_status_for_duplicated_positions(
+        check_traps_log_with_duplicated_positions, active_days
+    )
+    expected_days_for_trap_007 = 14
+    assert len(obtained[obtained["ID"] == "10-007"]) == expected_days_for_trap_007
