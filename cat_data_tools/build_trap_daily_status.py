e439a9dd57bbd7561a3f66bd985ca3cf179782ec
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 21:34:00 +0000

    ♻️ Refactor

--- cat_data_tools/build_trap_daily_status.py
+++ cat_data_tools/build_trap_daily_status.py
@@ -2,11 +2,15 @@ import pandas as pd
 
 
 def build_trap_daily_status(check_traps_log_df):
+    days_active = 7
+    return xxbuild_trap_daily_status(check_traps_log_df, days_active)
+
+
+def xxbuild_trap_daily_status(check_traps_log_df, days_active):
     check_traps_log_df["Date"] = pd.to_datetime(check_traps_log_df["Date"])
     df = check_traps_log_df[check_traps_log_df["Trap_status"] != "X"].copy()
     df_captures = check_traps_log_df[check_traps_log_df["Trap_status"] == "X"].copy()
 
-    days_active = 7
     df["Date"] = df["Date"].apply(lambda x: pd.date_range(start=x, periods=days_active, freq="D"))
     df_exploded = df.explode("Date")
     df_exploded.drop_duplicates(subset=["ID", "Date"], keep="last", inplace=True)
