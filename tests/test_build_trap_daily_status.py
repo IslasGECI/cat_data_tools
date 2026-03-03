8c6b070206b46cf10b2c0421b43417b8a0b167dc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 22:03:18 +0000

    ♻️ Refactor

--- tests/test_build_trap_daily_status.py
+++ tests/test_build_trap_daily_status.py
@@ -18,3 +18,5 @@ def tests_build_trap_daily_status():
     assert len(obtained[obtained["ID"] == "10-004"]) == expected_days_for_trap_004
     expected_days_for_trap_005 = 12
     assert len(obtained[obtained["ID"] == "10-005"]) == expected_days_for_trap_005
+    expected_days_for_trap_006 = 12
+    assert len(obtained[obtained["ID"] == "10-006"]) == expected_days_for_trap_006
