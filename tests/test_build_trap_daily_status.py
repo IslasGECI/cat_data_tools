c2ae16edf98f48c6a8bdfa94dcc573f744537992
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 22:06:14 +0000

    🛑🧪 Fail tests

--- tests/test_build_trap_daily_status.py
+++ tests/test_build_trap_daily_status.py
@@ -20,3 +20,5 @@ def tests_build_trap_daily_status():
     assert len(obtained[obtained["ID"] == "10-005"]) == expected_days_for_trap_005
     expected_days_for_trap_006 = 12
     assert len(obtained[obtained["ID"] == "10-006"]) == expected_days_for_trap_006
+    expected_last_day_trap_006 = "2025-01-12"
+    assert obtained[obtained["ID"] == "10-006"].iloc[-1] == expected_last_day_trap_006
