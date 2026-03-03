cd249b806552e0dafe00051c2e9aff834cb47bc0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 22:58:24 +0000

    ✅ Pass tests

--- cat_data_tools/build_trap_daily_status.py
+++ cat_data_tools/build_trap_daily_status.py
@@ -26,6 +26,6 @@ def build_trap_daily_status_for_duplicated_positions(check_traps_log_df, days_ac
     df_exploded.drop_duplicates(subset=["ID", "Date", "Type", "Trapper"], keep="last", inplace=True)
     df_actives = df_exploded[df_exploded["Trap_status"] == "A"].copy()
     concatenated_catpures = pd.concat([df_actives, df_captures]).drop_duplicates(
-        subset=["ID", "Date", "Type"], keep="last"
+        subset=["ID", "Date", "Type", "Trapper"], keep="last"
     )
     return concatenated_catpures.sort_values(by=["ID", "Date"]).reset_index(drop=True)
