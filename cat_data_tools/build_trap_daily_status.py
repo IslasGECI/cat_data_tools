2829b8990088d6d912c0f2af7410536034b27ace
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 22:56:04 +0000

    ✅ Pass tests

--- cat_data_tools/build_trap_daily_status.py
+++ cat_data_tools/build_trap_daily_status.py
@@ -8,6 +8,21 @@ def build_trap_daily_status(check_traps_log_df, days_active):
 
     df["Date"] = df["Date"].apply(lambda x: pd.date_range(start=x, periods=days_active, freq="D"))
     df_exploded = df.explode("Date")
+    df_exploded.drop_duplicates(subset=["ID", "Date", "Type"], keep="last", inplace=True)
+    df_actives = df_exploded[df_exploded["Trap_status"] == "A"].copy()
+    concatenated_catpures = pd.concat([df_actives, df_captures]).drop_duplicates(
+        subset=["ID", "Date", "Type"], keep="last"
+    )
+    return concatenated_catpures.sort_values(by=["ID", "Date"]).reset_index(drop=True)
+
+
+def build_trap_daily_status_for_duplicated_positions(check_traps_log_df, days_active):
+    check_traps_log_df["Date"] = pd.to_datetime(check_traps_log_df["Date"])
+    df = check_traps_log_df[check_traps_log_df["Trap_status"] != "X"].copy()
+    df_captures = check_traps_log_df[check_traps_log_df["Trap_status"] == "X"].copy()
+
+    df["Date"] = df["Date"].apply(lambda x: pd.date_range(start=x, periods=days_active, freq="D"))
+    df_exploded = df.explode("Date")
     df_exploded.drop_duplicates(subset=["ID", "Date", "Type", "Trapper"], keep="last", inplace=True)
     df_actives = df_exploded[df_exploded["Trap_status"] == "A"].copy()
     concatenated_catpures = pd.concat([df_actives, df_captures]).drop_duplicates(
