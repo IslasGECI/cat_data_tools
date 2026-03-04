eebccede3adc4a28468688b999cce7202a21ad39
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-04 20:05:43 +0000

    ✅ Pass tests

--- cat_data_tools/calculate_effort_and_captures.py
+++ cat_data_tools/calculate_effort_and_captures.py
@@ -1,5 +1,11 @@
 def calculate_effort_and_captures(data):
     data_copy = data.copy()
+    data_copy["is_capture"] = data_copy["Trap_status"] == "X"
     data_copy["Zone"] = data_copy["ID"].str.split("-").str[0]
-    summary = data_copy.groupby(["Date", "Zone"]).agg(Effort=("ID", "size")).reset_index()
+    summary = (
+        data_copy.groupby(["Date", "Zone"])
+        .agg(Effort=("ID", "size"), Captures=("is_capture", "sum"))
+        .reset_index()
+    )
+
     return summary
