78ec398956d74b7aa2b9e6e4437b50c2cc13f252
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-04 16:48:19 +0000

    ♻️ Refactor

--- cat_data_tools/build_trap_daily_status.py
+++ cat_data_tools/build_trap_daily_status.py
@@ -18,9 +18,12 @@ def build_trap_daily_status_for_duplicated_positions(check_traps_log_df, days_ac
 
 
 def fill_trap_daily_status(check_traps_log_df, days_active, unique_trap_identificators):
+    check_traps_log_df["Date"] = pd.to_datetime(check_traps_log_df["Date"])
+    df_without_captures = check_traps_log_df[check_traps_log_df["Trap_status"] != "X"].copy()
     df_actives = activate_traps_for_n_days(
-        check_traps_log_df, days_active, unique_trap_identificators
+        df_without_captures, days_active, unique_trap_identificators
     )
+
     df_captures = check_traps_log_df[check_traps_log_df["Trap_status"] == "X"].copy()
     concatenated_captures_sorted = concatenate_captures_and_actives(
         df_captures, df_actives, unique_trap_identificators
@@ -28,12 +31,11 @@ def fill_trap_daily_status(check_traps_log_df, days_active, unique_trap_identifi
     return concatenated_captures_sorted
 
 
-def activate_traps_for_n_days(check_traps_log_df, days_active, unique_trap_identificators):
-    check_traps_log_df["Date"] = pd.to_datetime(check_traps_log_df["Date"])
-    df = check_traps_log_df[check_traps_log_df["Trap_status"] != "X"].copy()
-
-    df["Date"] = df["Date"].apply(lambda x: pd.date_range(start=x, periods=days_active, freq="D"))
-    df_exploded = df.explode("Date")
+def activate_traps_for_n_days(df_without_captures, days_active, unique_trap_identificators):
+    df_without_captures["Date"] = df_without_captures["Date"].apply(
+        lambda x: pd.date_range(start=x, periods=days_active, freq="D")
+    )
+    df_exploded = df_without_captures.explode("Date")
 
     df_exploded.drop_duplicates(subset=unique_trap_identificators, keep="last", inplace=True)
     df_actives = df_exploded[df_exploded["Trap_status"] == "A"].copy()
