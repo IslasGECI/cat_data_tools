4ba93213e1850bac327a4e5f60623d7e000d4e75
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-04 18:37:58 +0000

    🛑🧪 Fail tests

--- tests/test_detect_status_change.py
+++ tests/test_detect_status_change.py
@@ -6,5 +6,5 @@ import pandas as pd
 def tests_get_status_change():
     daily_status_df = pd.read_csv("tests/data/daily_status_two_traps.csv")
     obtained = get_status_change(daily_status_df)
-    expected_status_changes = 23
+    expected_status_changes = 22
     assert len(obtained) == expected_status_changes
