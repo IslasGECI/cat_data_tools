4ce31a3caa9525bf7027fee5bbe6f0ec2758142a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-04 19:47:25 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_effort_and_captures.py
+++ tests/test_calculate_effort_and_captures.py
@@ -6,5 +6,5 @@ import pandas as pd
 def test_calculate_effort_and_captures():
     daily_status = pd.read_csv("tests/data/reconstructed_daily_status.csv")
     obtained = calculate_effort_and_captures(daily_status)
-    expected_rows = 12
+    expected_rows = 11
     assert len(obtained) == expected_rows
