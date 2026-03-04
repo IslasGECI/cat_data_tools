485ece82c92e9632d83c2eb2d351e062de690c11
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-04 18:26:55 +0000

    ✅ Pass tests

--- cat_data_tools/detect_status_change.py
+++ cat_data_tools/detect_status_change.py
@@ -1,5 +1,7 @@
 def get_status_change(daily_status_df):
     df = daily_status_df.sort_values(by=["ID", "Date"])
     is_status_changed = df["Trap_status"] != df.groupby("ID")["Trap_status"].shift()
-    filtered_df = df.loc[is_status_changed]
+    is_trapper_changed = df["Trapper"] != df.groupby("ID")["Trapper"].shift()
+    is_type_changed = df["Type"] != df.groupby("ID")["Type"].shift()
+    filtered_df = df.loc[is_status_changed | is_trapper_changed | is_type_changed]
     return filtered_df.reset_index(drop=True)
