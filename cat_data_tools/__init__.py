f4a94c04d2fbc718a654decaaafb8a70ba1a1d2e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-05 16:09:29 +0000

    ♻️ Refactor

--- cat_data_tools/__init__.py
+++ cat_data_tools/__init__.py
@@ -1,6 +1,6 @@
 """A Python module to process cats data"""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 from .cli import *  # noqa
 from .filter_data_between_years import *  # noqa
 from .filter_data_by_month import *  # noqa
