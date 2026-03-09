f87538dd8e80f542cf2cbaa23001088c14a22aff
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-09 20:16:37 +0000

    ♻️ Refactor 🐛 upgrade patch number version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -175,7 +175,7 @@ def test_app_write_monthly_summary():
         app,
         ["version"],
     )
-    assert "1.0.1" in result.stdout
+    assert "1.0.2" in result.stdout
     assert result.exit_code == 0
 
     command = "write-monthly-summary"
