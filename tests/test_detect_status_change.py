8b7fed1ee2ed740f1d8e889dc7fdef6f68cb11bd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-24 18:36:29 +0000

    🛑🧪 Fail tests

--- tests/test_detect_status_change.py
+++ tests/test_detect_status_change.py
@@ -1,4 +1,4 @@
-from cat_data_tools.detect_status_change import get_status_change
+from cat_data_tools.detect_status_change import add_next_status_column, get_status_change
 
 import pandas as pd
 
@@ -8,3 +8,9 @@ def tests_get_status_change():
     obtained = get_status_change(daily_status_df)
     expected_status_changes = 22
     assert len(obtained) == expected_status_changes
+
+
+def test_add_next_status_column():
+    daily_status_df = pd.read_csv("tests/data/daily_status_with_r_in_check.csv")
+    obtained = add_next_status_column(daily_status_df)
+    assert obtained.loc[0, "next_status"] == "A"
