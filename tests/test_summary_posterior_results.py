01ed7c3e34c92f2c121f2b6d8cc1e60e3bd7f8cc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-25 18:41:23 +0000

    🛑🧪 Fail tests

---
+++ tests/test_summary_posterior_results.py
@@ -0,0 +1,8 @@
+from summary_posterior_results import compute_critical_effort
+import pandas as pd
+
+
+def test_compute_critical_effort():
+    posterior_samples = pd.read_csv("tests/data/posterior_results.csv")
+    obtained = compute_critical_effort(posterior_samples)
+    assert isinstance(obtained, float)
