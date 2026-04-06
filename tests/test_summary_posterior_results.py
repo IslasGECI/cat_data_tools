a597abf63040ee67adb41c6839f8fd073e33fa30
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-06 15:36:06 +0000

    🛑🧪 Fail tests

--- tests/test_summary_posterior_results.py
+++ tests/test_summary_posterior_results.py
@@ -29,6 +29,7 @@ def test_compute_population_size_time_series():
     expected_series_length = 35
     obtained = compute_population_size_time_series(posterior_samples, effort_captures_df)
     assert len(obtained["population_size"]) == expected_series_length
+    assert len(obtained["population_size_percentile_95"]) == expected_series_length
     assert len(obtained["Date"]) == expected_series_length
     assert isinstance(obtained["population_size"], list)
 
