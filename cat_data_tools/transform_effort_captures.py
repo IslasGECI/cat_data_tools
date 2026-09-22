20a38cd8dee68765f61787c7d21509b79336ebdc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-09-22 21:16:16 +0000

    ✅ Pass tests

--- cat_data_tools/transform_effort_captures.py
+++ cat_data_tools/transform_effort_captures.py
@@ -1,8 +1,8 @@
-def transform_effort_captures_to_dict(df):
+def transform_effort_captures_to_dict(df, effort="esfuerzo", captures="capturas"):
     df["Esfuerzo"] = df.Esfuerzo.replace(0, 1)
     effort_captures_dict = (
         df[["Esfuerzo", "Capturas"]]
-        .rename(columns={"Esfuerzo": "esfuerzo", "Capturas": "capturas"})
+        .rename(columns={"Esfuerzo": effort, "Capturas": captures})
         .to_dict(orient="list")
     )
     effort_captures_dict["T"] = len(df)
@@ -10,4 +10,11 @@ def transform_effort_captures_to_dict(df):
 
 
 def transform_effort_and_captures_from_cameras_and_traps_to_dict(df_traps, df_cameras):
-    pass
+    traps_dict = transform_effort_captures_to_dict(
+        df_traps, effort="traps_effort", captures="captures"
+    )
+    cameras_dict = transform_effort_captures_to_dict(
+        df_cameras, effort="cameras_effort", captures="detections"
+    )
+    traps_dict.update(cameras_dict)
+    return traps_dict
