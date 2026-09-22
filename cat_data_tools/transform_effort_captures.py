9db6549bd89bb7999db30a4311b050250728385f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-09-22 21:43:42 +0000

    ✅ Pass tests

--- cat_data_tools/transform_effort_captures.py
+++ cat_data_tools/transform_effort_captures.py
@@ -10,11 +10,15 @@ def transform_effort_captures_to_dict(df, effort="esfuerzo", captures="capturas"
 
 
 def transform_effort_and_captures_from_cameras_and_traps_to_dict(df_traps, df_cameras):
-    traps_dict = transform_effort_captures_to_dict(
-        df_traps, effort="traps_effort", captures="captures"
+    df_traps["Esfuerzo"] = df_traps.Esfuerzo.replace(0, 1)
+    df_cameras["Esfuerzo"] = df_cameras.Esfuerzo.replace(0, 1)
+    df_traps = df_traps[["Esfuerzo", "Capturas", "Fecha"]].rename(
+        columns={"Esfuerzo": "traps_effort", "Capturas": "captures"}
     )
-    cameras_dict = transform_effort_captures_to_dict(
-        df_cameras, effort="cameras_effort", captures="detections"
-    )
-    traps_dict.update(cameras_dict)
-    return traps_dict
+    df_cameras = df_cameras.rename(columns={"Esfuerzo": "cameras_effort", "Capturas": "detections"})
+    df_traps.set_index("Fecha", inplace=True)
+    df_cameras.set_index("Fecha", inplace=True)
+    combined = df_traps.join(df_cameras, how="inner")
+    combined_dict = combined.to_dict(orient="list")
+    combined_dict["T"] = len(combined)
+    return combined_dict
